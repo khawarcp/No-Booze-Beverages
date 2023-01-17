ef270d8d5d3e2105ee955416a6a3d734acd28b1d
khawarcp <khawaraneescp@gmail.com>
2023-01-17 19:10:19 +0500

    custom product wrapper slider added

---
+++ assets/owl-product-slider.js
@@ -0,0 +1,19 @@
+// Product Slider
+$('.owl-carousel.prodSlider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
